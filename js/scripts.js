d05f2e6fda9fff6356e98a05e7283258ff03c745
Cryyli <98853857+Cryyli@users.noreply.github.com>
2023-01-12 11:24:53 -0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+/*!
+* Start Bootstrap - New Age v6.0.5 (https://startbootstrap.com/theme/new-age)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+$(document).ready(function() {
+  // Error Message
+  $(".btn-error").click(function(){
+    alert("Oh No! Please login to save");
+  });
+
+  // Show Table
+  $(".click1").click(function(){
+    $(".row3").slideDown('slow');
+  });
+
+})
+
+  
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
